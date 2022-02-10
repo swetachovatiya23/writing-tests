50a6e0dca49d7de6289ab343c6d14b0dd15eda7f
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-02-09 19:50:11 -0500

    trying my first test

---
+++ src/App.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react"
+import App from './App'
+ import userEvent from '@testing-library/user-event' 
+
+test("header renders with correct text", () => {
+  render(<App />)
+  const headerEl = screen.getByRole("heading")
+  expect(headerEl.textContent).toBe("Testing Playground")
+})
+
+
+  test("checkbox disables button when first clicked, then enables button on second click", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+    const checkbox = screen.getByRole("checkbox")
+  
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeDisabled()
+  
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeEnabled()
+  })
